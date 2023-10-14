0e457549df494b13f07000efa7bd6bf4690b02e5
Dan Yang <dan@Dans-MacBook-Pro.local>
2023-10-14 18:57:46 -0500

    Display names

--- src/app/box.tsx
+++ src/app/box.tsx
@@ -15,10 +15,10 @@ interface MatchData {
 }
 
 function Box(props: any) {
-  const [isExpanded, setIsExpanded] = useState(false);
+  // const [isExpanded, setIsExpanded] = useState(false);
   const [matches, setMatches] = useState([]);
   const [matchInfo, setMatchInfo] = useState({}); // maybe delete
-  const [matchesLookup, setMatchesLookup] = useState({});
+  const [matchesLookup, setMatchesLookup] = useState<MatchDataLookup>({});
   const [allMatches, setAllMatches] = useState<any>([]);
 
   useEffect(() => {
@@ -36,16 +36,16 @@ function Box(props: any) {
     // setMatchesLookup(dictionary);
   }, [allMatches])
 
-  const clickHandlerExpander = () => {
-    let newState = !isExpanded;
-    setIsExpanded(newState);
-    if (newState) getMatches(props.data.puuid);
-  }
+  // const clickHandlerExpander = () => {
+  //   let newState = !isExpanded;
+  //   setIsExpanded(newState);
+  //   if (newState) getMatches(props.data.puuid);
+  // }
 
-  const getMatches = async (userId: any) => {
-    let matchesForUser = await getMatchesForUser(userId);
-    setMatches(matchesForUser);
-  }
+  // const getMatches = async (userId: any) => {
+  //   let matchesForUser = await getMatchesForUser(userId);
+  //   setMatches(matchesForUser);
+  // }
 
   const getAllMatches = async () => {
     matches.forEach(async (matchId: string) => {
@@ -66,46 +66,45 @@ function Box(props: any) {
 
   return (
     <>
-      <div className="shadowy" onClick={clickHandlerExpander}>
-        {/* <h1 className="d-flex justify-content-between">
+      <div className="shadowy">
+        <h1 className="d-flex justify-content-between">
           <span className=''>{props.name}</span>
-          <span className=''>{props.data.summonerLevel}</span></h1> */}
+          <span className=''>{props.data.summonerLevel}</span>
+          </h1>
         {
-          isExpanded
-            ?
-            <Col>
-              <Card>
-                <Card.Header as="h5">{props.name}</Card.Header>
-                <Card.Body>
-                  <Card.Title>Special title treatment</Card.Title>
-                  <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            // <div className='container'>
-            //   <div className='col'>
-            //     {matches.map(x => <div key={x}>{matches.length > 0 ? x : ''}</div>)}
-            //   </div>
-            //   <div className='col'>
-            //     {
-            //       Object.keys(matchesLookup).length > 0 && matches.length > 0
-            //         ?
-            //         matches.map((x: string) =>
-            //           <div key={x}>
-            //             {/* {matchesLookup[x] ? 'min: ' + matchesLookup[x].gameDuration/60 : ''} */}
-            //             {matchesLookup[x] ? matchesLookup[x].participants.filter(x => x.summonerId === props.data.id)[0].win ? 'W' : '' : ''}
-            //             {/* {Object.keys(matchesLookup).length} */}
-            //           </div>)
-            //         :
-            //         ''
-            //     }
-            //     {/* {matchInfo.info.participants.filter(x => x.summonerId === props.data.id)[0].win} */}
-            //   </div>
-            // </div>
-            : ''
+            // <Col>
+            //   <Card>
+            //     <Card.Header as="h5">{props.name}</Card.Header>
+            //     <Card.Body>
+            //       <Card.Title>Special title treatment</Card.Title>
+            //       <Card.Text>
+            //         With supporting text below as a natural lead-in to additional content.
+            //       </Card.Text>
+            //       <Button variant="primary">Go somewhere</Button>
+            //     </Card.Body>
+            //   </Card>
+            // </Col>
+            <div className='container'>
+              <div className='col'>
+                {matches.map(x => <div key={x}>{matches.length > 0 ? x : ''}</div>)}
+              </div>
+              <div className='col'>
+                {
+                  Object.keys(matchesLookup).length > 0 && matches.length > 0
+                    ?
+                    matches.map((x: string) =>
+                      <div key={x}>
+                        {/* {matchesLookup[x] ? 'min: ' + matchesLookup[x].gameDuration/60 : ''} */}
+                        {matchesLookup[x] ? matchesLookup[x].participants.filter((x: any) => x.summonerId === props.data.id)[0].win ? 'W' : '' : ''}
+                        {/* {Object.keys(matchesLookup).length} */}
+                      </div>)
+                    :
+                    ''
+                }
+                {/* {matchInfo.info.participants.filter(x => x.summonerId === props.data.id)[0].win} */}
+              </div>
+            </div>
+
         }
       </div>
     </>
