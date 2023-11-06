4e1b6e6b48362171ccb3f8fe037a55bb996a1b43
Dan Yang <dan@Dans-MacBook-Pro.local>
2023-11-06 15:04:14 -0600

    Display more props like ping and firsts

--- src/app/DataBox.tsx
+++ src/app/DataBox.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useMemo } from 'react';
 // import { getMatchesForUser, getMatchData, getPlayerInfo } from './ApiCalls';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap/';
-// import * as status from './constants';
 import { atom, useAtom } from 'jotai';
 import { allMatchesAtom, matchesDetailsAtom, fullMatchesAtom } from './DataBoxState';
 import { hasChanges } from './MainPage';
@@ -42,112 +41,6 @@ function DataBox({ playerName }: PlayerName) {
     setData(latest);
   }
 
-  // useEffect(() => {
-  //   getPlayerObject();
-  // }, []);
-
-  // useEffect(() => {
-  //   getAllMatches()
-  // }, [matches]);
-
-  // useEffect(() => {
-  //   createLookupObj();
-  // }, [allMatches])
-
-  // const getPlayerObject = async () => {
-  //   const playerObject = await getPlayerInfo(playerName)
-  //   getMatchIds(playerObject.puuid);
-  // }
-
-  // const getMatchIds = async (userId: any) => {
-  //   if(!userId) return;
-
-  //   let matchIdsForUser = await getMatchesForUser(userId);
-  //   setMatches(matchIdsForUser);
-  //   setMatchIdsForAllUsers(existing => Array.from(new Set([...existing, ...matchIdsForUser])));
-  // }
-  // interface MatchId {
-  //   matchId: string
-  // }
-  // const getAllMatches = async () => {
-  //   if(matchIdsForAllUsers.length === 0) return;
-  //   let allMatchesForUser: any[] = [];
-  //   // console.log(matchIdsForAllUsers);
-  //   // console.log(matches);
-
-  //   let existing = localStorage.getItem('fullMatches') || '[]';
-  //   let getThese = Object.keys(JSON.parse(existing));
-  //   let missing = matchIdsForAllUsers.filter(item => getThese.indexOf(item) < 0);
-  //   missing.forEach(async (matchId: string) => {
-  //     // console.log('matchDetailsForAllUsers', Object.keys(matchDetailsForAllUsers).length)
-  //     console.log('match found locally!', localMatches[matchId]);
-  //     let matchInfo = localMatches[matchId] || await getMatchData(matchId);
-  //     // let matchInfo = localMatches[matchId]
-  //     allMatchesForUser.push(matchInfo);
-  //     setAllMatches((currData: any) => [...currData, matchInfo]);
-  //     setHasChanged(true);
-  //   })
-  //   if(missing.length === 0) {
-  //     setAllMatches(localMatches);
-  //   }
-  // }
-
-  // const createLookupObj = () => {
-  //   if(allMatches.length === 0) return; 
-
-  //   let dictionary: MatchDataLookup = {};
-  //   Object.values(allMatches).forEach((match: any) => {
-  //     if(match !== status.API_ERR) {
-  //       dictionary[match.metadata.matchId] = match;
-  //       // setMatchDetailsForAllUsers({...matchDetailsForAllUsers, [match.metadata.matchId]: match.info})
-  //     }
-  //   })
-  //   setMatchDetailsForAllUsers({...matchDetailsForAllUsers, ...dictionary})
-  //   // console.log('Atom', matchDetailsForAllUsers);
-  //   // console.log('Dict', dictionary);
-  //   setMatchesLookup(dictionary);
-    
-  //   let current = {...localMatches, ...matchDetailsForAllUsers};
-  //   let hasChanges = Object.keys(current).length > Object.keys(localMatches).length;
-  //   if (hasChanges) {
-  //     localStorage.setItem('fullMatches', JSON.stringify(current));
-  //   };
-  // }
-
-  // interface DisplayedData {
-  //   id: string;
-  //   date: string;
-  //   result: boolean;
-  //   champion: string;
-  //   kda: number;
-  //   level: number;
-  //   alive: string;
-  //   timeAway: string;
-  // }
-
-  // const prepareDataForTable = (data: any)  => {
-  //   console.log('prepareDataForTable', Object.keys(data).length === Object.keys(JSON.parse(localStorage.fullMatches)).length);
-  //   if (matches.length === 0 || Object.keys(matchesLookup).length === 0) return [];
-
-  //   const playerData = matches.filter(matchId => {
-  //     return matchesLookup[matchId];
-  //   }).map(matchId => {
-  //     let data = matchesLookup[matchId];
-  //     let playerInGameData = data.info.participants.find((x: any) => x.summonerName === playerName)
-  //     return {
-  //       id: matchId,
-  //       date: formatDate(data.info.gameCreation),
-  //       result: playerInGameData.win ? 'W' : 'L',
-  //       champion: `${playerInGameData.championName}`,
-  //       kda: `${playerInGameData.kills}/${playerInGameData.deaths}/${playerInGameData.assists}`,
-  //       level: playerInGameData.champLevel,
-  //       alive: formatMinutes(playerInGameData.longestTimeSpentLiving),
-  //       timeAway: formatMinutes(playerInGameData.totalTimeSpentDead)
-  //     }
-  //   })
-  //   return playerData;
-  // }
-
   const prepareDataForTable2 = (datas: any)  => {
     console.log('prepareDataForTable', Object.keys(datas).length, Object.keys(JSON.parse(localStorage.fullMatches)).length, Object.keys(localMatches).length);
     // console.log('----------', localMatches)
@@ -158,7 +51,8 @@ function DataBox({ playerName }: PlayerName) {
       return localMatches[matchId] && playerInGameData;
     }).map(matchId => {
       let data = localMatches[matchId];
-      let playerInGameData = localMatches[matchId].info.participants.find((x: any) => x.summonerName === playerName);
+      let playerInGameData = data.info.participants.find((x: any) => x.summonerName === playerName);
+      const teamId = data.info.teams.find((x: any) => x.teamId === playerInGameData?.teamId)?.teamId;
       if (!playerInGameData) return {};
       return {
         id: matchId,
@@ -169,10 +63,23 @@ function DataBox({ playerName }: PlayerName) {
         kda: `${playerInGameData.kills}/${playerInGameData.deaths}/${playerInGameData.assists}`,
         level: playerInGameData.champLevel,
         alive: formatMinutes(playerInGameData.longestTimeSpentLiving),
-        timeAway: formatMinutes(playerInGameData.totalTimeSpentDead)
+        timeAway: formatMinutes(playerInGameData.totalTimeSpentDead),
+        surrender: playerInGameData.gameEndedInEarlySurrender || playerInGameData.gameEndedInSurrender ? 'Yes' : '',
+        firstKill: playerInGameData.firstBloodKill ? 'Yes' : '',
+        firstTower: data.info.participants.filter((x: any) => x.teamId === teamId).some((x: any) => x.firstBloodKill) ? 'Yes': '',
+        pings: getNumberOfPings(playerInGameData)
       }
     })
     return playerData;
+  }
+
+  const getNumberOfPings = (data: any) => {
+    const datas: { [key: string]: any } = {};
+    constants.PINGS.forEach((ping: string) => datas[ping] = data[ping]);
+    const num = constants.PINGS.map(x => datas[x]);
+    const sum = num.reduce((a,b) => a+b, 0);
+    console.log(sum);
+    return sum;
   }
 
   const formatMinutes = (seconds: number) => (seconds/60).toFixed() + "'" 
--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -3,3 +3,20 @@ export const GAMES_TO_SHOW = 10;
 export const ERR_CODE = "ERR_NETWORK";
 export const CANT_FIND = "Can't find user";
 export const API_ERR = "API Error";
+
+export const PINGS = [
+    'allInPings',
+    'assistMePings',
+    'baitPings',
+    'basicPings',
+    'commandPings',
+    'dangerPings',
+    'enemyMissingPings',
+    'enemyVisionPings',
+    'getBackPings',
+    'holdPings',
+    'needVisionPings',
+    'onMyWayPings',
+    'pushPings',
+    'visionClearedPings'
+]
