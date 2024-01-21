6986cb525609d2f4aae91f89d04c8636d3877d2e
Dan Yang <dan@Dans-MacBook-Pro.local>
2024-01-21 15:45:16 -0600

    Added routing

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,13 +3,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import NxWelcome from './nx-welcome';
 import MainPage from './MainPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NxWelcome from './nx-welcome';
+import Layout from './Layout';
 
 export function App() {
   return (
-    <>
-      {/* <NxWelcome title="league-app" /> */}
-      <MainPage></MainPage>
-    </>
+    // <>
+    //   {/* <NxWelcome title="league-app" /> */}
+    //   <MainPage></MainPage>
+    // </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="abc" element={<NxWelcome title={'Hola'} />} />
+        <Route path="*" element={<MainPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
