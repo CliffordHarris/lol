a51dfa4eaa4db46b5fa93ef9c0a267d30a82627b
Dan Yang <dan@Dans-MacBook-Pro.local>
2024-02-04 14:47:24 -0600

    Add type to comp stats

--- src/app/CompareStats.tsx
+++ src/app/CompareStats.tsx
@@ -1,33 +1,35 @@
 import { getDataForN } from "./Util"; // brings in functions and files
 
-
-function CompareStats() { // component name
-
-const myGames = getDataForN(); // logic
-
-console.log(myGames[2])
-
-
-
-for (let i = 0; i < myGames.length; i++) {
-  const g = myGames[i];
-  let listOfStats = [];
-  listOfStats.push(
-    {
-      name: g["championName"],
-      position: g["individualPosition"],
-      experience: g["champExperience"],
-      visionScore: g["visionScore"],
-      damageDealt: g["totalDamageDealt"]
-    }
-    );
-
-  console.table(listOfStats)
+type Stats = {
+  name: string;
+  position: string;
+  experience: number;
+  visionScore: number;
+  damageDealt: number;
 }
 
+function CompareStats() { // component name
+  const myGames: any[] = getDataForN(); // logic
+  
+  console.log(myGames[2]);
 
-return ( // returns html
-    <div style={{background: "red"}}>
+  for (let i = 0; i < myGames.length; i++) {
+    const game = myGames[i];
+    let listOfStats: Stats[] = [];
+    let aStatObject = {
+      name: game["championName"],
+      position: game["individualPosition"],
+      experience: game["champExperience"],
+      visionScore: game["visionScore"],
+      damageDealt: game["totalDamageDealt"],
+    };
+    listOfStats.push(aStatObject);
+
+    console.table(listOfStats)
+  }
+
+  return ( // returns html
+    <div style={{ background: "red" }}>
       <div className="text-center">
         Compare Stats
       </div>
@@ -40,18 +42,18 @@ return ( // returns html
     </div>
   );
 }
- 
+
 export default CompareStats;
 
 // 1. Compare each teams stats based on roles
 // 2. Get data:
-  // a. KDA
-  // b. Kill participation
-  // c. Ability usage
-  // d. Characters
-  // e. Character Level
+// a. KDA
+// b. Kill participation
+// c. Ability usage
+// d. Characters
+// e. Character Level
 // 3. Design:
-  // a. Left side is team one
-  // b. Right side is team two
-  // c. Role matchup
-  // d. Winning side green
+// a. Left side is team one
+// b. Right side is team two
+// c. Role matchup
+// d. Winning side green
