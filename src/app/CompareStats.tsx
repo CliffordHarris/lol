e0722ac9681c95f1025ee22793c782af4024e2f3
unknown <=>
2024-02-04 15:34:46 -0600

    printed data in html

--- src/app/CompareStats.tsx
+++ src/app/CompareStats.tsx
@@ -6,6 +6,7 @@ type Stats = {
   experience: number;
   visionScore: number;
   damageDealt: number;
+  win: string;
 }
 
 function CompareStats() { // component name
@@ -13,31 +14,42 @@ function CompareStats() { // component name
   
   console.log(myGames[2]);
 
+  let listOfStats: Stats[] = [];
+
   for (let i = 0; i < myGames.length; i++) {
     const game = myGames[i];
-    let listOfStats: Stats[] = [];
     let aStatObject = {
       name: game["championName"],
       position: game["individualPosition"],
       experience: game["champExperience"],
       visionScore: game["visionScore"],
       damageDealt: game["totalDamageDealt"],
+      win: game["win"] ? "win" : "lose"
     };
     listOfStats.push(aStatObject);
 
-    console.table(listOfStats)
   }
+  console.table(listOfStats)
 
   return ( // returns html
-    <div style={{ background: "red" }}>
+    <div style={{ background: "lightgray"}}>
       <div className="text-center">
         Compare Stats
       </div>
       <div>
-        Team 1
+        Nat
       </div>
-      <div className="text-end">
-        Team 2
+      <div>
+        {listOfStats.map(stat => (
+          <div>
+            <h2>{stat.name}</h2>
+            <div>{stat.position}</div>
+            <div>{stat.experience}</div>
+            <div>{stat.visionScore}</div>
+            <div>{stat.damageDealt}</div>
+            <div>{stat.win}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
