e6d9c4cb68940bd9692da030d8b75983a15e5728
lexvue <alexandertvue@gmail.com>
2024-02-11 15:14:33 -0600

    Got item names to show instead of item ids

--- src/app/UserItems.tsx
+++ src/app/UserItems.tsx
@@ -3,28 +3,33 @@ import DataTable from './DataTable';
 import { getItemDictionaryForItemIds } from "./ApiCalls";
 
 function UserItems() {
-    const items = ['item0', 'item1', 'item2', 'item3', 'item4', 'item5', "item6"];
+    const itemStringList = ['item0', 'item1', 'item2', 'item3', 'item4', 'item5', "item6"];
     const listOfItems = [];
     //const formatMinutes = (seconds: number) => (seconds/60).toFixed() + "'";
     const myGames = getDataForA();
-    console.log(getItemDictionaryForItemIds([1052])); // Amplifying Tome item id
+    console.log(getItemDictionaryForItemIds([1052, 3020])); // Amplifying Tome item id
+
 
     // filter
-
-    
     for (let i = 0; i < myGames.length; i++) {
         const games = myGames[i];
+    
+        // DRY - Don't Repeat Yourself
+    
+        const itemIds: number[] = itemStringList.map(s => games[s]);
+        const itemDict = getItemDictionaryForItemIds(itemIds); // Amplifying Tome item id
+
         listOfItems.push(
           {
             id: i,
             championName: games["championName"],
-            item0: games["item0"],
-            item1: games["item1"],
-            item2: games["item2"],
-            item3: games["item3"],
-            item4: games["item4"],
-            item5: games["item5"],
-            item6: games["item6"],
+            item0: itemDict[games["item0"]],
+            item1: itemDict[games["item1"]],
+            item2: itemDict[games["item2"]],
+            item3: itemDict[games["item3"]],
+            item4: itemDict[games["item4"]],
+            item5: itemDict[games["item5"]],
+            item6: itemDict[games["item6"]],
             controlWardsPlaced: games["challenges"]["controlWardsPlaced"],
             controlWardTimeCoverage: games["challenges"]["controlWardTimeCoverageInRiverOrEnemyHalf"],
             visionScoreAdvantage: games["challenges"]["visionScoreAdvantageLaneOpponent"],
@@ -36,6 +41,7 @@ function UserItems() {
         console.table(listOfItems);
       }
 
+      // This code renders html
     return (
         <>
             
