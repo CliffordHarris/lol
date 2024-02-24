97daf1767cba562d7e3b37adbb2700205334df4c
lexvue <alexandertvue@gmail.com>
2024-02-24 14:01:20 -0600

    Added a function to show an emoji in place of a missing item

--- src/app/UserItems.tsx
+++ src/app/UserItems.tsx
@@ -9,6 +9,11 @@ function UserItems() {
     const myGames = getDataForA();
     console.log(getItemDictionaryForItemIds([1052, 3020])); // Amplifying Tome item id
 
+    // create a func 
+    // return "-" or something like that when that spot in the items section isnt occupied
+    const formatGameItem = (itemDict: any, games: any, item: string) => {
+        return games[item] ? itemDict[games[item]] : '😂';
+    };
 
     // filter
     for (let i = 0; i < myGames.length; i++) {
@@ -19,17 +24,19 @@ function UserItems() {
         const itemIds: number[] = itemStringList.map(s => games[s]);
         const itemDict = getItemDictionaryForItemIds(itemIds); // Amplifying Tome item id
 
+        // Grab the image png to show that with item names
+
         listOfItems.push(
           {
             id: i,
             championName: games["championName"],
-            item0: itemDict[games["item0"]],
-            item1: itemDict[games["item1"]],
-            item2: itemDict[games["item2"]],
-            item3: itemDict[games["item3"]],
-            item4: itemDict[games["item4"]],
-            item5: itemDict[games["item5"]],
-            item6: itemDict[games["item6"]],
+            item0: formatGameItem(itemDict, games, "item0"),
+            item1: formatGameItem(itemDict, games, "item1"),
+            item2: formatGameItem(itemDict, games, "item2"),
+            item3: formatGameItem(itemDict, games, "item3"),
+            item4: formatGameItem(itemDict, games, "item4"),
+            item5: formatGameItem(itemDict, games, "item5"),
+            item6: formatGameItem(itemDict, games, "item6"),
             controlWardsPlaced: games["challenges"]["controlWardsPlaced"],
             controlWardTimeCoverage: games["challenges"]["controlWardTimeCoverageInRiverOrEnemyHalf"],
             visionScoreAdvantage: games["challenges"]["visionScoreAdvantageLaneOpponent"],
@@ -41,6 +48,7 @@ function UserItems() {
         console.table(listOfItems);
       }
 
+
       // This code renders html
     return (
         <>
@@ -49,7 +57,9 @@ function UserItems() {
                 (item) =>
                     <div key={item.id}>
                         <div>{item.championName} ITEMS: </div>
-                            <ul>
+                            <ol>
+                                {/* // 2. I want to show the items in the league format like > [1] [2] [3]
+//                                                                                             [4] [5] [6] */}
                                 <li>{item.item0}</li>
                                 <li>{item.item1}</li>
                                 <li>{item.item2}</li>
@@ -66,7 +76,7 @@ function UserItems() {
                                         <li>Vision Score Per Minute: {item.visionScorePerMinute}</li>
                                     </ul>
                                 </div>
-                            </ul>
+                            </ol>
                         
 
                     </div>
@@ -78,7 +88,5 @@ function UserItems() {
 export default UserItems;
 
 // 1. I want the id numbers of the items to show in place of "item#"
-// 2. I want to show the items in the league format like > [1] [2] [3]
-//                                                         [4] [5] [6]
 // 3. I want to show what stats were improved by the items bought
 // 4. Sight Wards bought/placed/destroyed
